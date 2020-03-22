9db3d268786642943dfdf1161690497e81daaf85
Adam Hall <adamhalldesigns@gmail.com>
2020-03-22 21:51:08 +0000

    simple chat

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-import app from './app';
+import express from 'express';
+import http from 'http';
+import socket from 'socket.io';
 
-console.log('🐼', app); // eslint-disable-line
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(`${process.cwd()}/public/index.html`);
+});
+
+io.on('connection', (sock) => {
+  sock.on('chat message', (msg: string) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, function () {
+  console.log('listening on *:3000');
+});
